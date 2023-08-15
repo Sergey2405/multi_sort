6c75f6d17e735f07bee89c7191d3dc1d3e8680b5
Sergey Grachev <grachevse2405@gmail.com>
2023-08-15 21:54:40 +0300

    Use argv[1] rebase

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,10 @@ int main( int argc, char *argv[], char *envp[] )
     array_size = atoi(argv[1]);
     std::cout << "start array_size=" << array_size << std::endl;
     MinMaxSort minMaxSort;
-    std::cout << "minMaxSort" << std::endl;
     MultiSort* pMultiSort = &minMaxSort;
-    std::cout << "pMultiSort" << std::endl;
 
     pMultiSort->sort();
     std::cout << "sort() duration:" << pMultiSort->get_duration().count() << std::endl;
-    
+
     return 0;
 }
