6507a2f0bb52a80ff3669623623fabc6858cf97f
Sergey Grachev <grachevse2405@gmail.com>
2023-09-04 23:53:23 +0300

    Quick sort works but needs cosmetics corrections

--- src/quick_sort.cpp
+++ src/quick_sort.cpp
@@ -11,26 +11,51 @@ QuickSort::~QuickSort()
 
 void QuickSort::sort(int startIx, int endIx)
 {
-    std::cout << "QuickSort::sort " << startIx << "Ix " << endIx << "Ix ";
-    printArray(); std::cout << std::endl;
+    // std::cout << "QuickSort::sort " << startIx << "Ix " << endIx << "Ix "; printArray(); std::cout << std::endl;
 
-    if (!(startIx < endIx && startIx >= 0)) {
-        std::cout << "return\n";
+    // if (!(startIx < endIx && startIx >= 0)) {
+    if (!(startIx < endIx)) {
+        // std::cout << "return0" << startIx << "Ix " << endIx << "Ix "; printArray(); std::cout << std::endl;
         return;
     }
-    int pivot = m_piRandomArray[endIx],
-        lowerIx = startIx -1,
-        upperIx = startIx;
-    while (upperIx < endIx && lowerIx < upperIx) {
-        if (m_piRandomArray[upperIx] > pivot) {
+    if (startIx + 1 == endIx) {
+        if (m_piRandomArray[startIx] > m_piRandomArray[endIx])
+            std::swap(m_piRandomArray[startIx], m_piRandomArray[endIx]);
+        // std::cout << "return+1 " << startIx << "Ix " << endIx << "Ix "; printArray(); std::cout << std::endl;
+        return;
+    }
+    int pivot = m_piRandomArray[(startIx + endIx) / 2],
+        pivotIx = (startIx + endIx) / 2,
+        lowerIx = startIx,
+        upperIx = startIx + 1;
+    while (!(upperIx > endIx)) {
+        // std::cout << "while " << lowerIx << "Ix " << upperIx << "Ix "; printArray(); std::cout << std::endl;
+        if (!(m_piRandomArray[upperIx] > pivot)) {
+            while (lowerIx < upperIx) {
+                // std::cout << "while2 " << lowerIx << "Ix " << upperIx << "Ix "; printArray(); std::cout << std::endl;
+                if (m_piRandomArray[lowerIx] > m_piRandomArray[upperIx]) {
+                    // std::cout << "swap2 " << lowerIx << "Ix " << upperIx << "Ix "; printArray(); std::cout << std::endl;
+                    // if (lowerIx == pivotIx)
+                    //     pivot = upperIx;
+                    // if (upperIx == lowerIx)
+                    //     pivotIx == upperIx;
+                    std::swap(m_piRandomArray[lowerIx], m_piRandomArray[upperIx]);
+                }
+                if (m_piRandomArray[lowerIx] < pivot) {
+                    lowerIx++;
+                    continue;
+                }
+                break;
+            }
             upperIx++;
-            break;
+            continue;
         }
-        std::swap(m_piRandomArray[++lowerIx], m_piRandomArray[upperIx]);
+        upperIx++;
     }
 
-    sort(startIx, lowerIx - 1);
-    sort(lowerIx, endIx);
+    // pivotIx = std::min(pivotIx, lowerIx);
+    sort(startIx, lowerIx);
+    sort(lowerIx + 1, endIx);
 
 }
 
@@ -38,10 +63,6 @@ std::chrono::microseconds QuickSort::sort()
 {
     auto startClock = std::chrono::high_resolution_clock::now();
     sort(0, m_iSize - 1);
-    // for (int i = 0; i < m_iSize; i++)
-    //     for (int j = 0; j < m_iSize - 1; j++)
-    //         if (m_piRandomArray[j] > m_piRandomArray[j + 1])
-    //             std::swap(m_piRandomArray[j], m_piRandomArray[j + 1]);
     auto endClock = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(endClock - startClock);
     m_duration = duration;
