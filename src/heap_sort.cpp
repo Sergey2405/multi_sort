1033f04574ef601485850567ccca6b6ed7e855da
Sergey Grachev <grachevse2405@gmail.com>
2023-11-21 21:05:22 +0300

    Add counters

--- src/heap_sort.cpp
+++ src/heap_sort.cpp
@@ -24,13 +24,14 @@ void HeapSort<T>::make_heap()
         lowerIx = std::min(HeapSort<T>::m_iSize - 1, (int)pow(2, currentLevel));
         for (; lowerIx > 0; lowerIx--) {
             int upperIx = (lowerIx + 1) / 2 - 1;
-            if (MultiSort<T>::m_randomArray[lowerIx] > MultiSort<T>::m_randomArray[upperIx])
+            if (MultiSort<T>::m_randomArray[lowerIx] > MultiSort<T>::m_randomArray[upperIx]) {
                 std::swap(MultiSort<T>::m_randomArray[lowerIx], MultiSort<T>::m_randomArray[upperIx]);
+                MultiSort<T>::m_iSwapCounter++;
+            }
+            MultiSort<T>::m_iCompareCounter++;
+            MultiSort<T>::m_iFetchCounter += 2;
         }
     }
-    // std::cout << "HeapSort<T>::make_heap. print array: ";
-    // this->printArray();
-
     make_heap(0);
 }
 
@@ -42,11 +43,16 @@ void HeapSort<T>::make_heap(int a_SortedCounter)
         lowerIx = HeapSort<T>::m_iSize - a_SortedCounter - 1;
     for (; lowerIx > 0; lowerIx--) {
         upperIx = (lowerIx + 1) / 2 - 1;
-        if (MultiSort<T>::m_randomArray[lowerIx] > MultiSort<T>::m_randomArray[upperIx])
+        if (MultiSort<T>::m_randomArray[lowerIx] > MultiSort<T>::m_randomArray[upperIx]) {
             std::swap(MultiSort<T>::m_randomArray[lowerIx], MultiSort<T>::m_randomArray[upperIx]);
+                            MultiSort<T>::m_iSwapCounter++;
+        }
+        MultiSort<T>::m_iCompareCounter++;
+        MultiSort<T>::m_iFetchCounter += 2;
     }
     if(HeapSort<T>::m_iSize > a_SortedCounter + 1){
         std::swap(MultiSort<T>::m_randomArray[0], MultiSort<T>::m_randomArray[HeapSort<T>::m_iSize - a_SortedCounter - 1]);
+        MultiSort<T>::m_iSwapCounter++;
         make_heap(a_SortedCounter + 1);
     }
 }
